9d7d771a37826f14c6f77de276c518ad6353ceb1
Rahul Imran <notlutitious@gmail.com>
2022-10-10 18:11:33 +0100

    Feat: Add a basic things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Thing(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
